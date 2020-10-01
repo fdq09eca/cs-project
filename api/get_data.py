6a2e6720dde1ed926fe6697fcae59ac59b3304e6
Thanh Bui <macone@Macs-MacBook-Pro.local>
2020-10-01 18:34:50 +0800

    api

--- api/get_data.py
+++ api/get_data.py
@@ -1,9 +1,6 @@
 import json, requests, datetime, html, logging
 from typing import Generator
 from collections import namedtuple
-from os import sys, path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from helper import over_a_year, yesterday, today, n_yearsago
 
 class HKEX_API:
     
@@ -26,7 +23,7 @@ class HKEX_API:
     }
 
     
-    def __init__(self, from_date, to_date, stock_id=None, doc='annual_report'):
+    def __init__(self, from_date=None, to_date=None, stock_id=None, doc='annual_report'):
         self.from_date = from_date
         self.to_date = to_date
         self.stock_id = stock_id
@@ -40,7 +37,8 @@ class HKEX_API:
     
     @from_date.setter
     def from_date(self, from_date:str) -> str:
-        self._from_date = self.date_fmt_validator(from_date, HKEX_API.dt_fmt)
+        from_date = from_date or HKEX_API.n_yearsago(n=1)
+        self._from_date = self.date_fmt_validator(from_date)
     
     
     @property
@@ -49,55 +47,19 @@ class HKEX_API:
     
     @to_date.setter
     def to_date(self, to_date:str) -> str:
-        self._to_date = self.date_fmt_validator(to_date, HKEX_API.dt_fmt)
-    
-    
+        to_date = to_date or HKEX_API.today()
+        self._to_date = self.date_fmt_validator(to_date)
+
     @property
     def payloads(self) -> dict:
-        return self.set_payloads(from_date=self.from_date, to_date=self.to_date, stock_id=self.stock_id, doc=self.doc)
         
-    @property
-    def data(self) -> Generator['namedtuple', None, None]:
-        return (i for i in self.get_data())
+        from_date=self.from_date
+        to_date=self.to_date
+        stock_id=self.stock_id
+        doc=self.doc
 
-    
-    @staticmethod
-    def data_decoder(data):
-        data = {k.lower(): html.unescape(v) for k, v in data.items()}
-        data['file_link'] = "https://www1.hkexnews.hk" + data['file_link']
-        return namedtuple('data', data.keys())(*data.values())
-    
-    
-    @staticmethod
-    def date_fmt_validator(date_str:str, dt_fmt:str='%Y%m%d') -> None:
-        try:
-            datetime.datetime.strptime(date_str, dt_fmt)
-            return date_str
-        except ValueError as e:
-            logging.critical(f'{e}, date str format is not {dt_fmt}')
-            raise ValueError(f'{e}, date str format is not {dt_fmt}')
-    
-    @staticmethod
-    def call_api(endpoint:str, payloads:dict) -> tuple:
-               
-        with requests.get(endpoint, params=payloads) as response:
-            response.raise_for_status()
-            site_json = json.loads(response.text)
-            if site_json['hasNextRow']:
-                payloads['rowRange'] = site_json['recordCnt']
-                return HKEX_API.call_api(endpoint, payloads=payloads)
-            results = json.loads(site_json['result'], object_hook = HKEX_API.data_decoder)
-            return tuple(results)
-    
-
-    @staticmethod
-    def set_payloads(from_date:str, to_date:str=today('%Y%m%d'), stock_id:str=None, doc:str=None) -> dict:
-               
-        HKEX_API.date_fmt_validator(from_date, '%Y%m%d')
-        HKEX_API.date_fmt_validator(to_date, '%Y%m%d')
-
-        if over_a_year(from_date=from_date) and stock_id is None:
-            ytd = n_yearsago(1)
+        if HKEX_API.over_a_year(from_date=from_date) and stock_id is None:
+            ytd = HKEX_API.n_yearsago(1)
             raise ValueError(f'Query over a year must specify stock_id e.g "1", global query can only from "{ytd}"')
 
         payloads = {
@@ -120,17 +82,86 @@ class HKEX_API:
         
         return payloads
 
-    def get_data(self) -> list:
-        return HKEX_API.call_api(endpoint=HKEX_API.endpoint, payloads=self.payloads)
+
+    @staticmethod
+    def data_decoder(data):
+        data = {k.lower(): html.unescape(v) for k, v in data.items()}
+        data['file_link'] = "https://www1.hkexnews.hk" + data['file_link']
+        return namedtuple('data', data.keys())(*data.values())
+    
+    
+    @staticmethod
+    def date_fmt_validator(date_str:str) -> None:
+        try:
+            datetime.datetime.strptime(date_str, HKEX_API.dt_fmt)
+            return date_str
+        except ValueError as e:
+            raise ValueError(f'{e}, {date_str} format is not {HKEX_API.dt_fmt}')
     
 
+    @staticmethod
+    def call_api(endpoint:str, payloads:dict) -> tuple:
+               
+        with requests.get(endpoint, params=payloads) as response:
+            response.raise_for_status()
+            site_json = json.loads(response.text)
+            if site_json['hasNextRow']:
+                payloads['rowRange'] = site_json['recordCnt']
+                return HKEX_API.call_api(endpoint, payloads=payloads)
+            results = json.loads(site_json['result'], object_hook = HKEX_API.data_decoder)
+            return tuple(results)
+    
+
+    @staticmethod
+    def yesterday() -> str:
+        '''
+        return yesterday string value in HKEX_API.dt_fmt format
+        '''
+        dt_yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        fmt_yesterday = dt_yesterday.strftime(HKEX_API.dt_fmt)
+        return fmt_yesterday
+
+    @staticmethod
+    def today() -> str:
+        '''
+        return today string value in HKEX_API.dt_fmt format
+        '''
+        dt_today = datetime.date.today()
+        fmt_today = dt_today.strftime(HKEX_API.dt_fmt)
+        return fmt_today
+
+    @staticmethod
+    def over_a_year(from_date, to_date=datetime.date.today()) -> bool:
+        '''
+        return if from_date to to_date is over a year 
+        '''
+        ytd = HKEX_API.n_yearsago(1, to_date)
+        datetime_ytd = datetime.datetime.strptime(ytd, HKEX_API.dt_fmt)
+        datetime_frm_date = datetime.datetime.strptime(from_date, HKEX_API.dt_fmt)
+        return datetime_frm_date < datetime_ytd
+
+    @staticmethod
+    def n_yearsago(n:int, from_date=datetime.datetime.now()) -> str:
+        try:
+            dt_yearsago = from_date.replace(year=from_date.year - n)
+        except ValueError:
+            # Must be 2/29!
+            assert from_date.month == 2 and from_date.day == 29  # can be removed
+            dt_yearsago = from_date.replace(month=2, day=28, year=from_date.year - n)
+        finally:
+            return dt_yearsago.strftime(HKEX_API.dt_fmt)
+    
+    def get_data(self) -> list:
+        return HKEX_API.call_api(endpoint=HKEX_API.endpoint, payloads=self.payloads)
     
 
 if __name__ == '__main__':
     # pass
     # print(_get_data())
     # query = HKEX_API(from_date=yesterday(), to_date=today())
-    query = HKEX_API(from_date=n_yearsago(n=1), to_date=today())
+    # query = HKEX_API(from_date=n_yearsago(n=1), to_date=today())
+    # query = HKEX_API(from_date=HKEX_API.n_yearsago(n=1), to_date=HKEX_API.today())
+    query = HKEX_API()
     print(query.doc)
     print(query.payloads['t2code'])
     print(len(query.get_data()))
@@ -144,7 +175,7 @@ if __name__ == '__main__':
     print(len([i for i in query.data]))
     print('>>>>>>')
     print(query.from_date)
-    query.from_date = yesterday()
+    query.from_date = HKEX_API.yesterday()
     print(query.from_date)
     print(query.payloads['fromDate'])
     print(len(query.get_data()))
