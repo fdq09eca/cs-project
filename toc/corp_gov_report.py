aff9763040e245fb38562aa1723aaa241a80ce18
Thanh Bui <macone@Macs-MacBook-Pro.local>
2020-10-05 01:48:12 +0800

    good header

--- toc/corp_gov_report.py
+++ toc/corp_gov_report.py
@@ -39,10 +39,9 @@ class AuditFeeTable:
         "horizontal_strategy": "text"
     }
 
-    currency_regx = 'HK'
+    currency_regex = 'HK'
     financial_num_regex = '\d,?\d*'
-    year_regx = '|'.join(map(str, range(
-        int(datetime.datetime.now().year) - 2, int(datetime.datetime.now().year) + 1)))
+    year_regex = '|'.join(map(str, range(int(datetime.datetime.now().year) - 2, int(datetime.datetime.now().year) + 1)))
 
     def __init__(self, section):
         self.section = section
@@ -68,7 +67,7 @@ class AuditFeeTable:
 
     @staticmethod
     def currency_element(x):
-        return x.str.contains(AuditFeeTable.currency_regx)
+        return x.str.contains(AuditFeeTable.currency_regex)
 
     @staticmethod
     def financial_num_element(x):
@@ -77,23 +76,69 @@ class AuditFeeTable:
     @staticmethod
     def year_element(x):
         return x.str.contains(AuditFeeTable.year_regex)
+    
+    @staticmethod
+    def eng_element(x):
+        return x.str.contains(r'[A-Za-z0-9]+')
+    
+    @staticmethod
+    def get_cond_cols(df, element_cond_func):
+        return df.apply(element_cond_func).any()
+    
+    @staticmethod
+    def get_cond_rows(df, element_cond_func):
+        return df.apply(element_cond_func).any(axis=1)
+    
+    @property
+    def df_fee(self):
+        df = self.df_raw_table
+        currency_cols = self.get_cond_cols(df, self.currency_element)
+        currency_rows = self.get_cond_rows(df, self.currency_element)
+        financial_num_rows = self.get_cond_rows(df, self.financial_num_element)
+        year_rows = self.get_cond_rows(df, self.year_element)
+        df_fee = df.loc[(financial_num_rows|year_rows) , currency_cols]
+        return df_fee
 
+   
+    @property
+    def df_filtered_table(self):
+        df_fee = self.df_fee
+        df_filtered_table = self.df_raw_table.iloc[df_fee.index]
+        df_filtered_table = df_filtered_table.loc[:, self.get_cond_cols(df_filtered_table, self.eng_element)]
+        return df_filtered_table
+    
+    
     @property
     def df_clean_table(self):
-        df = self.df_raw_table
-        currency_rows = df.apply(self.currency_element).any(axis=1)
-        financial_num_rows = df.apply(self.financial_num_element).any(axis=1)
-        year_rows = df.apply(self.year_element).any(axis=1)
-        df = df[currency_rows | financial_num_rows]
-        df = df.loc[:, df.apply(
-            lambda x: x.str.contains(r'[A-Za-z0-9]+')).any()]
-        return df
+        df_filtered_table = self.df_filtered_table
+        currency_cols = self.get_cond_cols(df_filtered_table, self.currency_element)
+        financial_num_cols = self.get_cond_cols(df_filtered_table, self.financial_num_element)
+        currency_rows= self.get_cond_rows(df_filtered_table, self.currency_element)
+        year_rows = self.get_cond_rows(df_filtered_table, self.year_element)
 
-    @property
-    def df_currency(self):
-        df = self.df_clean_table
-        df = df.loc[:, df.apply(lambda x: x.str.contains(
-            AuditFeeTable.currency_regx)).any()]
+
+        currency_row_idx = df_filtered_table.loc[currency_rows, financial_num_cols].index
+        currency_row = df_filtered_table.loc[currency_row_idx]
+        currency_header = currency_row[currency_row.apply(self.currency_element, axis=1)].fillna('')
+        
+        year_row_idx = df_filtered_table.loc[year_rows, currency_cols].index
+        year_row = df_filtered_table.loc[year_row_idx]
+        year_header = year_row[year_row.apply(self.year_element, axis=1)].fillna('')
+
+        # print(f'year_header: {year_header}')
+        # print(f'currency_header: {currency_header}')
+
+        # return year_header
+        if not year_header.empty and not currency_header.enpty:
+        #     print(f'year_header: {year_header}')
+            df_filtered_table.columns = pd.MultiIndex.from_arrays([year_header.iloc[0], currency_header.iloc[0]], names = ['year', 'currency'])
+        else:
+            # print(f'currency_header: {currency_header}')
+            df_filtered_table.columns = pd.MultiIndex.from_arrays([currency_header.iloc[0]], names=['currency'])
+        # print(df_filtered_table)
+        return df_filtered_table.drop(year_row_idx.append(currency_row_idx))
+        # return df_filtered_table
+
 
     @property
     def currency(self):
@@ -121,6 +166,7 @@ if __name__ == '__main__':
     page = corp_gov_report.audit_fee.pages[0]
     sec = corp_gov_report.audit_fee.sections[0]
     table = corp_gov_report.audit_fee.tables[0]
+    print(table.df_clean_table)
     print(sec.text)
     # print(corp_gov_report.pages)
 
